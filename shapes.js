7631ecb8ed3131c05ed07f5f6d7d60925578e017
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-01-04 14:52:53 +0100

    shapes: optional circle strokeStyle

--- shapes.js
+++ shapes.js
@@ -5,8 +5,10 @@ export function circle(context, x, y, radius, options = {}) {
   context.save();
   context.beginPath();
   context.arc(x, y, radius, 0, TWO_PI, false);
-  context.strokeStyle = strokeStyle;
-  context.stroke();
+  if (strokeStyle) {
+    context.strokeStyle = strokeStyle;
+    context.stroke();
+  }
   if (fillStyle) {
     context.fillStyle = fillStyle;
     context.fill();
