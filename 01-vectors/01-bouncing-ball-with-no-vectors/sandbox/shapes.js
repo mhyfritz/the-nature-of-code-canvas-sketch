84fdf36b5de0942fda560c7207d123765ef3148c
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-01-04 16:20:26 +0100

    add `<head>` tag to sandbox template
    seems it doesn't parse it otherwise

--- 01-vectors/01-bouncing-ball-with-no-vectors/sandbox/shapes.js
+++ 01-vectors/01-bouncing-ball-with-no-vectors/sandbox/shapes.js
@@ -7,8 +7,11 @@ export function circle(context, x, y, radius, options = {}) {
   context.save();
   context.beginPath();
   context.arc(x, y, radius, 0, TWO_PI, false);
-  context.strokeStyle = strokeStyle;
-  context.stroke();
+
+  if (strokeStyle) {
+    context.strokeStyle = strokeStyle;
+    context.stroke();
+  }
 
   if (fillStyle) {
     context.fillStyle = fillStyle;
